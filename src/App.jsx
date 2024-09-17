db59b89fb218d3e4104b305d3c74aafe271e0404
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-17 20:59:38 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-function App() {
-  const initialFriends = [
-    {
-      id: 118836,
-      name: "Clark",
-      image: "https://i.pravatar.cc/48?u=118836",
-      balance: -7,
-    },
-    {
-      id: 933372,
-      name: "Sarah",
-      image: "https://i.pravatar.cc/48?u=933372",
-      balance: 20,
-    },
-    {
-      id: 499476,
-      name: "Anthony",
-      image: "https://i.pravatar.cc/48?u=499476",
-      balance: 0,
-    },
-  ];
-
-  return <div className=""></div>;
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
+  );
 }
 
-export default App;
+function FriendList() {
+  return <ul>list</ul>;
+}
